8f022f566d49526abcd3f7721f40ab5ddc23a85a
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2019-01-13 16:36:37 +0900

    require login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,10 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required
+
   def new
+  end
+
+  def create
     user = User.find_by(email: session_params[:email])
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
@@ -7,6 +12,11 @@ class SessionsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました。'
   end
 
   private
