9179df800b531504726ccd6381216ec99d633e8e
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2019-01-13 17:02:07 +0900

    bundle exec annotate

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :bigint(8)        not null, primary key
+#  name        :string(30)       not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint(8)        not null
+#
+
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :bigint(8)        not null, primary key
+#  name        :string(30)       not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint(8)        not null
+#
+
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
